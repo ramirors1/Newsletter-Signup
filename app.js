a317d90a999a3dedf21cde5011a9f1dfa9a944fa
Your Github User Name <ramirezr@protonmail.com>
2022-03-14 17:59:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+//needed in order to specify location of static files (images/css)
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+  
+  });
+
+  app.post("/", function(req, res) {
+
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+
+  });
+
+//sets up server
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
